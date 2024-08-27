e380e8e525ee82a09f23a25628385f99d79d56f3
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-08-27 23:33:05 +0100

    sql

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """ 10-update_topics.py """
 
+
 def update_topics(mongo_collection, name, topics):
     """
     Updates the topics of a school document 
     based on the name.
     """
-     mongo_collection.update_many(
-             {'name': name},
-             {'$set': {'topics': topics}}
-             )
+    mongo_collection.update_many(
+            {'name': name},
+            {'$set': {'topics': topics}}
+            )
